92c507a0a9e5d84bf81d884a284cc581e0fbead2
bwapen1 <147283127+bwapen1@users.noreply.github.com>
2024-01-02 03:37:58 -0800

    Update config.js

--- config.js
+++ config.js
@@ -17,11 +17,11 @@ global.statusseen = true;                 //This is the new variable for control
 
 
 //
-global.Owner = ["46493656","46493656"];         //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
-global.OwnerNumber = ["46493656","46493656"];   //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
+global.Owner = ["31107344","31107344"];         //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
+global.OwnerNumber = ["31107344","31107344"];   //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
 global.ownertag = ["55413561"];
-global.OwnerName = "Mr_Babas";
-global.BotName = "bwa_pen";
+global.OwnerName = "ANDY>XYZ";
+global.BotName = "bwa_pen.BOT;
 global.packname = "X-2.0 Bot";                             //Do not change.
 global.author = "By: Slasher";                               //Do not change.
 global.BotSourceCode = "https://github.com/ahil15/Xlicon-v2"; //Do not change.
